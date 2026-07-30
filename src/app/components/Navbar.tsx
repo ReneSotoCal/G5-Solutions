3f6694a2c040b4db5882dbefe01eddae7619b603
Rene Soto Calzadilla <rs11659@georgiasouthern.edu>
2026-07-29 20:23:03 -0400

    Updated styling to look more modern Updated dependencies to mitigate vulnerabilities

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
                     </div>
                     <Link 
                         href="/" 
-                        className="space-x-6 order-2 block px-4 py-2 hover:bg-slate-800 hover:text-red-600 active:text-red-600 rounded font-medium tracking-wide transition-colors duration-200"
+                        className="space-x-6 order-2 block px-4 py-2 hover:bg-slate-800 hover:text-red-600 active:text-red-600 rounded-lg font-medium tracking-wide transition-colors duration-200"
                         >Home</Link>
                     <div
                         onMouseOver={() => setIsDrpDwnOpen(true)}
@@ -51,28 +51,28 @@ export default function Navbar() {
                         className='relative order-3 flex items-center justify-center'>
                         <Link 
                             href="/#services-section" 
-                            className="space-x-6 block px-4 py-2 hover:bg-slate-800 hover:text-red-600 rounded active:text-red-600 font-medium tracking-wide transition-colors duration-200"
+                            className="space-x-6 block px-4 py-2 hover:bg-slate-800 hover:text-red-600 rounded-lg active:text-red-600 font-medium tracking-wide transition-colors duration-200"
                             >Services</Link>
                             {isDrpDwnOpen && (
-                                <div className={`absolute text-center top-8 mt-4 w-68 bg-slate-900 text-white rounded-md shadow-lg p-4 space-y-2`}>
-                                <Link href="/services/CloudServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 rounded tracking-wide">Cloud Services</Link>
-                                    <Link href="/services/NetworkingServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 rounded tracking-wide">Networking & Infrastructure</Link>
-                                    <Link href="/services/SecurityServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 rounded tracking-wide">CyberSecurity</Link>
-                                    <Link href="/services/EndToEndServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 rounded tracking-wide">End-to-End IT Solutions</Link>
+                                <div className={`absolute text-center top-full w-68 bg-slate-900 text-white rounded-xl shadow-lg p-4 space-y-2`}>
+                                    <Link href="/services/CloudServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 rounded-lg tracking-wide">Cloud Services</Link>
+                                    <Link href="/services/NetworkingServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 rounded-lg tracking-wide">Networking & Infrastructure</Link>
+                                    <Link href="/services/SecurityServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 rounded-lg tracking-wide">CyberSecurity</Link>
+                                    <Link href="/services/EndToEndServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 rounded-lg tracking-wide">End-to-End IT Solutions</Link>
                                 </div>
                             )}
 
                     </div>
                     <Link 
                         href="/AboutUs" 
-                        className="space-x-6 order-4 block px-4 py-2 hover:bg-slate-800 hover:text-red-600 active:text-red-600 rounded font-medium tracking-wide cursor-pointer transition-colors duration-200"
+                        className="space-x-6 order-4 block px-4 py-2 hover:bg-slate-800 hover:text-red-600 active:text-red-600 rounded-lg font-medium tracking-wide cursor-pointer transition-colors duration-200"
                         >About Us</Link>
                     {isMobile ? <CTAButton onClick={openForm} className="hidden">Get Your Free Consultation</CTAButton> : <CTAButton onClick={openForm} className="order-5">Get Your Free Consultation</CTAButton>}
 
                 </div>
                 <div className="flex justify-between p-2">
                     <button
-                        className="md:hidden p-2 rounded-md hover:bg-slate-800 active:bg-slate-800"
+                        className="md:hidden p-2 rounded-lg hover:bg-slate-800 active:bg-slate-800"
                         onClick={() => setNavCollapsed(!navCollapsed)}>
                         {!navCollapsed ? <X className="w-6 h-6 hover:text-red-600 focus:text-red-600 active:text-red-600 cursor-pointer" /> : <Menu className="cursor-pointer w-6 h-6" />}
                     </button>
@@ -81,8 +81,8 @@ export default function Navbar() {
                 </div>
                 
                 {isMobile && !navCollapsed && (
-                    <div className="md:hidden flex flex-col p-4 space-y-4 bg-slate-900 text-white top-2">
-                    <div className="flex items-center">
+                    <div className="md:hidden flex flex-col text-center p-4 space-y-4 bg-slate-900 text-white top-2">
+                    <div className="flex items-center justify-center">
                         <Link href="/" onClick={() => setNavCollapsed(true)}>
                             <Image 
                             src="/g5-new-logo.png"
@@ -92,12 +92,12 @@ export default function Navbar() {
                             width={64}
                             />
                         </Link>
-                        <Link href="/" className="text-white text-xl font-semibold sm:inline pl-2">G5 Solutions</Link>
+                        <Link href="/" className="text-white text-xl font-semibold sm:inline">G5 Solutions</Link>
                     </div>
-                        <Link href="/" onClick={() => setNavCollapsed(true)} className="active:text-red-600 focus:text-red-600 hover:text-red-600">Home</Link>
-                            <div 
-                                className='relative hover:cursor-pointer hover:text-red-600 focus:text-red-600 active:text-red-600'>
-                                <div className="flex items-center space-x-1">
+                        <Link href="/" onClick={() => setNavCollapsed(true)} className="hover:bg-slate-800 active:bg-slate-800 focus:bg-slate-800 active:text-red-600 focus:text-red-600 hover:text-red-600 rounded-lg">Home</Link>
+                            <div
+                            className='relative hover:cursor-pointer hover:text-red-600 hover:rounded-lg focus:rounded-lg active:rounded-lg focus:text-red-600 active:text-red-600'>
+                            <div className="flex items-center justify-center space-x-1 pl-6 hover:bg-slate-800 active:bg-slate-800 focus:bg-slate-800 rounded-lg">
                                     <Link
                                         href="/#services-section"
                                         onClick={() => {setNavCollapsed(true); setIsDrpDwnOpen(false)}} 
@@ -105,25 +105,25 @@ export default function Navbar() {
                                     {
                                         isDrpDwnOpen ? 
                                         <ChevronUp 
-                                            className="inline pb-1" 
+                                            className="inline" 
                                             onClick={() => setIsDrpDwnOpen(false)}/>
                                         : <ChevronDown 
-                                            className="inline pb-1"
+                                            className="inline"
                                             onClick={() => setIsDrpDwnOpen(true)}/>
                                     }
                                     {isDrpDwnOpen && (
                                             <div 
-                                            className="absolute text-center z-55 top-10 w-56 bg-slate-900 text-white rounded-md shadow-lg p-4 space-y-2">
-                                                <Link href="/services/CloudServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 active:text-red-600 active:bg-slate-800 focus:text-red-600 focus:bg-slate-800 rounded font-medium tracking-wide" onClick={() => {setIsDrpDwnOpen(false); setNavCollapsed(true)}}>Cloud Services</Link>
-                                                <Link href="/services/NetworkingServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 active:text-red-600 active:bg-slate-800 focus:text-red-600 focus:bg-slate-800 rounded font-medium tracking-wide" onClick={() => {setIsDrpDwnOpen(false); setNavCollapsed(true)}}>Networking & Infrastructure</Link>
-                                                <Link href="/services/SecurityServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 active:text-red-600 active:bg-slate-800 focus:text-red-600 focus:bg-slate-800 rounded font-medium tracking-wide" onClick={() => {setIsDrpDwnOpen(false); setNavCollapsed(true)}}>CyberSecurity</Link>
-                                                <Link href="/services/EndToEndServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 active:text-red-600 active:bg-slate-800 focus:text-red-600 focus:bg-slate-800 rounded font-medium tracking-wide" onClick={() => {setIsDrpDwnOpen(false); setNavCollapsed(true)}}>End-to-End IT Solutions</Link>
+                                            className="absolute text-center z-55 top-full w-56 bg-slate-900 text-white rounded-xl shadow-lg space-y-2 p-4">
+                                                <Link href="/services/CloudServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 active:text-red-600 active:bg-slate-800 focus:text-red-600 focus:bg-slate-800 rounded-lg font-medium tracking-wide" onClick={() => {setIsDrpDwnOpen(false); setNavCollapsed(true)}}>Cloud Services</Link>
+                                                <Link href="/services/NetworkingServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 active:text-red-600 active:bg-slate-800 focus:text-red-600 focus:bg-slate-800 rounded-lg font-medium tracking-wide" onClick={() => {setIsDrpDwnOpen(false); setNavCollapsed(true)}}>Networking & Infrastructure</Link>
+                                                <Link href="/services/SecurityServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 active:text-red-600 active:bg-slate-800 focus:text-red-600 focus:bg-slate-800 rounded-lg font-medium tracking-wide" onClick={() => {setIsDrpDwnOpen(false); setNavCollapsed(true)}}>CyberSecurity</Link>
+                                                <Link href="/services/EndToEndServices" className="block px-4 py-2 hover:text-red-600 hover:bg-slate-800 active:text-red-600 active:bg-slate-800 focus:text-red-600 focus:bg-slate-800 rounded-lg font-medium tracking-wide" onClick={() => {setIsDrpDwnOpen(false); setNavCollapsed(true)}}>End-to-End IT Solutions</Link>
                                             </div>
                                         )}
                                 </div>
                             </div>
 
-                        <Link href="/AboutUs" onClick={() => setNavCollapsed(true)} className="active:text-red-600 focus:text-red-600 hover:text-red-600">About Us</Link>
+                        <Link href="/AboutUs" onClick={() => setNavCollapsed(true)} className="rounded-lg hover:bg-slate-800 active:bg-slate-800 focus:bg-slate-800 active:text-red-600 focus:text-red-600 hover:text-red-600">About Us</Link>
                     </div>
         )}
             </div>
